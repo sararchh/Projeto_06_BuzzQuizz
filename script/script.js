57e4b611f297adb92e6f43a32aefe0bd3f4f8cfe
Leonardo Osugi <leoosugi96@gmail.com>
2022-09-01 21:39:23 -0300

    Inicio do renderizamento de perguntas e respostas

--- script/script.js
+++ script/script.js
@@ -39,39 +39,65 @@ function renderizarTodosQuizzes() {
 
 //Buscando um QUIZZ para o usuário responder
 function buscarUmQuizz(){
-  const promisse = axios.get(`${urlBase}/quizzes/ID_DO_QUIZZ`);
+  const promisse = axios.get(`${urlBase}/quizzes/11204`);
   promisse.then(quizzChegou);
 }
 
+buscarUmQuizz();
+
 function quizzChegou(resposta) {
-  buscarQuizzes = resposta.data;
-  renderizarPerguntas();
+  buscarQuizz = resposta.data;
+  renderizarPerguntas(buscarQuizz);
 }
 
-function renderizarPerguntas(){
+function renderizarPerguntas(quizz){
   const ulPerguntas = document.querySelector('.respostaQuizz');
 
-  buscarQuizzes.map((issoBanner) =>(
-    ulPerguntas.innerHTML += `
-      <li>
-         <div class="banner">
-             <img src="${issoBanner.image}">
-             <div class="titulo-banner">
-                 <p>
-                    ${issoBanner.title}
-                 </p>
-             </div>
+  ulPerguntas.innerHTML += `
+  <li>
+     <div class="banner">
+         <img src="${quizz.image}">
+         <div class="titulo-banner">
+             <p>
+                ${quizz.title}
+             </p>
          </div>
-      </li>
-    `
+     </div>
+  </li>`;
     
-  ));
+    //Aqui formata a pergunta, é o questions
+    quizz.questions.forEach((pergunta) => {
+      ulPerguntas.innerHTML += `
+          <li>
+              <div>
+                 <div>
+                    ${pergunta.title}
+                 </div>
+              </div>
+          </li>
+        `;
 
-  questoes.map((pergunta) => (
-    ulPerguntas.innerHTML += `
+        // aqui é o answers
+    pergunta.answers.forEach((resposta)=> {
+      ulPerguntas.innerHTML += `
+      <li>
+          <div>
+             <div>
+                ${resposta.title}
+             </div>
+          </div>
+      </li>
+    `;
+    }) //fechamento foreach das respostas
 
-    `
-  ));
+    }) // fechamento foreach das perguntas
+
+    // aqui é o levels
+    quizz.levels.forEach((level)=>{
+      //aqui voce busca o level e faz o inner
+
+    }) //fechamento do foreach level
+
 }
 
 function pegaQuiz(){
