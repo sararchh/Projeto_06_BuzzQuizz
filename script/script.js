7287a0bc054c9e2088dfaaeaa73f55f64f3baaad
Rodolpho Piermattei Buettel <rodolpho05buettel@gmail.com>
2022-09-02 16:19:37 -0300

    Criação do quizz com uma pergunta e um nivel e objeto prontos

--- script/script.js
+++ script/script.js
@@ -15,6 +15,23 @@ let qntPer;
 let qntNiv;
 let imagemNova;
 
+let tituloDaPergunta1;
+let corDeFundo1;
+let respostaCerta;
+let imagemDaRespostaCerta;
+let respostaIncorreta1;
+let respostaIncorreta2;
+let respostaIncorreta3;
+let imagemIncorreta1;
+let imagemIncorreta2;
+let imagemIncorreta3; 
+
+let tituloDoNivel;
+let porcentagem;
+let imagemDoNivel;
+let descricaoDoNivel;
+let porcentagemMinima;
+
 setTimeout(buscarTodosQuizzes,1000);
 
 function buscarSeusQuizzes() {
@@ -109,6 +126,98 @@ function irParaPerguntas(){
       } 
     }
   }
+  
+}
+
+// condições para que a pergunta seja aceita 
+function irParaNiveis(){
+  tituloDaPergunta1 = document.querySelector('.tituloPergunta1').value;
+  corDeFundo1 = document.querySelector('.corDeFundo1').value;
+  respostaCerta = document.querySelector('.respostaCerta').value;
+  imagemDaRespostaCerta = document.querySelector('.imagemDaRespostaCerta').value
+  respostaIncorreta1 = document.querySelector('.respostaIncorreta1').value; 
+  respostaIncorreta2 = document.querySelector('.respostaIncorreta2').value;
+  respostaIncorreta3 = document.querySelector('.respostaIncorreta3').value;
+  imagemIncorreta1 = document.querySelector('.imagemIncorreta1').value;
+  imagemIncorreta2 = document.querySelector('.imagemIncorreta2').value;
+  imagemIncorreta3 = document.querySelector('.imagemIncorreta3').value;
+  if(tituloDaPergunta1.length < 20){
+    alert('O titulo tem que ter no minimo 20 letras');
+  }
+  else{
+    tituloDaPergunta1 = tituloDaPergunta1;
+    if(validaCor(corDeFundo1) === false){
+      alert('Insira uma cor hexadecimal');
+    }
+    else{
+      corDeFundo1 = corDeFundo1;
+      if(validURL(imagemDaRespostaCerta) === false){
+        alert('URL inválida');
+      }
+      else{
+        imagemDaRespostaCerta = imagemDaRespostaCerta;
+        if(respostaCerta === ''){
+          alert('Insira a resposta correta');
+        }
+        else{
+          respostaCerta = respostaCerta;
+          if(respostaIncorreta1 === '' && respostaIncorreta2 === '' 
+          && respostaIncorreta3 === ''){
+              alert('Insira pelo menos uma resposta errada');
+            }
+          else{
+            respostaIncorreta1 = respostaIncorreta1;
+            respostaIncorreta2 = respostaIncorreta2;
+            respostaIncorreta3 = respostaIncorreta3;
+            if(validURL(imagemIncorreta1) === false && validURL(imagemIncorreta2) === false
+              && validURL(imagemIncorreta3) === false){
+                alert('Insira uma URL válida');
+              }
+              else{
+                imagemIncorreta1 = imagemIncorreta1;
+                imagemIncorreta2 = imagemIncorreta2;
+                imagemIncorreta3 = imagemIncorreta3;
+                prosseguirParaNiveis();
+              }  
+          }
+        }
+      }
+    }
+   }
+}
+
+// condições para que os nivei seja aceito 
+function quizzCriado(){
+  tituloDoNivel = document.querySelector('.tituloDoNivel1').value;
+  porcentagem = document.querySelector('.porcentagemDoNivel1').value;
+  imagemDoNivel = document.querySelector('.imagemDoNivel1').value;
+  descricaoDoNivel = document.querySelector('.descricaoDoNivel1').value;
+  if(tituloDoNivel.length < 10){
+    alert('No minino 10 letras pro titulo');
+  }
+  else{
+    tituloDoNivel = tituloDoNivel;
+    if(porcentagem > 0 || porcentagem < 100){
+      alert('Porcentagem precisa estar entre 0 a 100');
+    }
+    else{
+      porcentagem = porcentagem;
+      if(validURL(imagemDoNivel) === false){
+        alert('URL inválida');
+      }
+      else{
+        imagemDoNivel = imagemDoNivel;
+        if(descricaoDoNivel.length < 30){
+          alert('Descrição no minímo de 30 letras');
+        }
+        else{
+          descricaoDoNivel = descricaoDoNivel;
+          criaObjetoDoQuizz();
+          finalizaQuizz();
+        }
+      }
+    }
+  }
 
 }
 
@@ -122,6 +231,53 @@ function validURL(str) {
   return !!pattern.test(str);
 }
 
+function validaCor(str){
+  var pattern = new RegExp("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$");
+  return !!pattern.test(str);
+}
+
+function criaObjetoDoQuizz(){
+  let quizzCriadoPeloUsuario = {
+    title: tituloNovo,
+    image: imagemNova,
+    questions: [
+      {
+        title: tituloDaPergunta1,
+        color: corDeFundo1,
+        answers: [
+          {
+            text: respostaCerta,
+            image: imagemDaRespostaCerta,
+            isCorrectAnsewrs: true
+          },
+          {
+            text: respostaIncorreta1,
+            image: imagemIncorreta1,
+            isCorrectAnswer: false
+          },
+          {
+            text: respostaIncorreta2,
+            image: imagemIncorreta2,
+            isCorrectAnswer: false
+          },
+          {
+            text: respostaIncorreta3,
+            image: imagemIncorreta3,
+            isCorrectAnswer: false
+          }
+        ],
+        levels:
+          {
+            title: tituloDoNivel,
+            image: imagemDoNivel,
+            text: descricaoDoNivel,
+            minValue: porcentagem
+          }
+      }
+    ]
+  }
+  console.log(quizzCriadoPeloUsuario);
+}
 // Habilita o usuario a prosseguir com a criação de perguntas 
 function prosseguir(){
   if(tituloNovo !== ''){
@@ -138,6 +294,24 @@ function prosseguir(){
         }
       }
     }
+}
+
+//Habilita o usario a prosseguir para os niveis 
+function prosseguirParaNiveis(){
+  let criaNiveis = document.querySelector('.perguntas');
+  criaNiveis.classList.add('escondida');
+
+  let niveis = document.querySelector('.niveis');
+  niveis.classList.remove('escondida');
+
+}
+//Habilita a tela final 
+function finalizaQuizz(){
+  let quizzFinalizado = document.querySelector('.niveis');
+  quizzFinalizado.classList.add('escondida');
+
+  let fimDoQuizz = document.querySelector('.telaFinal');
+  fimDoQuizz.classList.remove('escondida');
 }
 
 function renderizarPerguntas(quizz){
